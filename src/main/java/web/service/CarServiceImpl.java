d5f1d177cdc4097a594989cb13898c74eac255a9
IvanZhumabaev99 <107753325+IvanZhumabaev99@users.noreply.github.com>
2023-07-12 14:08:30 +0300

    fix: исправил по замечаниям CarServiceImpl; в классе CarController убрал явное создание экземпляра CarServiceImpl в конструкторе.

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -3,27 +3,27 @@ package web.service;
 import org.springframework.stereotype.Component;
 import web.model.Car;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 public class CarServiceImpl implements CarService {
 
-    @Override
-    public List<Car> getSpecifiedNumberOfCars(int a) {
-        List<Car> newListCar = new ArrayList<>();
-        for (int i = 0; i < a; i++){
-            newListCar.add(ListCar.get(i));
-        }
-        return newListCar;
-    }
+    private List<Car> carList = List.of(
+        new Car("Brand 1", "Color 1", 1),
+        new Car("Brand 2", "Color 2", 2),
+        new Car("Brand 3", "Color 3", 3),
+        new Car("Brand 4", "Color 4", 4),
+        new Car("Brand 5", "Color 5", 5)
+    );
 
-    private List<Car> ListCar;
-    {
-        ListCar = new ArrayList<>();
-        ListCar.add(new Car("Brand 1", "Color 1", 1));
-        ListCar.add(new Car("Brand 2", "Color 2", 2));
-        ListCar.add(new Car("Brand 3", "Color 3", 3));
-        ListCar.add(new Car("Brand 4", "Color 4", 4));
-        ListCar.add(new Car("Brand 5", "Color 5", 5));
+    @Override
+    public List<Car> getSpecifiedNumberOfCars(int count) {
+        if (count >= carList.size() || count < 0) {
+            return carList;
+        }
+            return carList.stream()
+                    .limit(count)
+                    .collect(Collectors.toList());
     }
 }
